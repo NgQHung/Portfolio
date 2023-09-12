2e558b128f016f0437fb813b0b7aa68a4e514e85
NgQHung <hunghunghung273@gmail.com>
2023-09-12 21:15:08 +0200

    icons added

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import React from 'react';
+import Logo from './Logo';
+import {useRouter} from 'next/router';
+import {GithubIcon, LinkedInIcon} from './icons';
+
+interface ICustomLink {
+    href: string;
+    title: string;
+    className?: string;
+}
+
+const CustomLink: React.FC<ICustomLink> = ({href, title, className = ''}) => {
+    const router = useRouter();
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+            <span
+                className={`h-1 inline-block w-0 bg-black absolute left-0 -bottom-0.5 
+                group-hover:w-full transition-[width] ease duration-300 
+                ${router.asPath === href ? 'w-full' : 'w-0'}`}>
+                &nbsp;
+            </span>
+        </Link>
+    );
+};
+
+const Navbar = () => {
+    return (
+        <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+            <nav>
+                <CustomLink href="/" title="Home" className="mr-4" />
+                <CustomLink href="/about" title="About" className="mx-4" />
+                <CustomLink href="/projects" title="Projects" className="mx-4" />
+                <CustomLink href="/articles" title="Articles" className="ml-4" />
+            </nav>
+
+            <nav>
+                <Link href="/" target={'_blank'}>
+                    <GithubIcon />
+                </Link>
+                <Link href="/" target={'_blank'}>
+                    <LinkedInIcon />
+                </Link>
+            </nav>
+            <div className="absolute  left-[50%] top-2 translate-x-[-50%] ">
+                <Logo />
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
